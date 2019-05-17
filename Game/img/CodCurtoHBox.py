d95772a49fa4fc389f4fc340961dea225de3edeb
vitorbandeira1 <49555280+vitorbandeira1@users.noreply.github.com>
2019-05-17 11:05:22 -0300

    CodCurtoHBox

--- Game/img/CodCurtoHBox.py
+++ Game/img/CodCurtoHBox.py
@@ -54,48 +54,6 @@ class player(pygame.sprite.Sprite):
         if self.rect.left < 0:
             self.rect.left = 0
 
-
-
-
-
-
-
-'''
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.vel = 5
-        self.pulo = False
-        self.left = False
-        self.right = False
-        self.walkCount = 0
-        self.jumpCount = 10
-        self.standing = True
-        self.hitbox = (self.x + 17, self.y + 11, 29, 52)
-        self.rect = pygame.Rect(self.x + 17, self.y + 11, 29, 52)
-        self.rect.x=width/2
-        self.rect.y=510
-        
-    def update(self,win):
-        if self.walkCount +1 >= 27:
-            self.walkCount = 0
-        if not(self.standing):
-            if self.left:
-                win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
-                self.walkCount +=1
-            elif self.right:
-                win.blit(walkRight[self.walkCount//3], (self.x,self.y))
-                self.walkCount +=1
-        else:
-            if self.right:
-                win.blit(walkRight[0], (self.x,self.y))
-            else:
-                win.blit(walkLeft[0], (self.x,self.y))
-            self.hitbox=self.hitbox = (self.x + 17, self.y + 11, 29, 52)
-            self.rect = pygame.Rect(self.x + 17, self.y + 11, 29, 52)
-            pygame.draw.rect(win,(255,0,0), self.hitbox,2)#para desenhar o hit box no boneco. ta no update pois tem q atualizar toda vez que ele anda
-'''
 class enemy(pygame.sprite.Sprite):
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
@@ -127,58 +85,6 @@ class enemy(pygame.sprite.Sprite):
         self.heroy = y
 
 
-#class inimigo(pygame.sprite.Sprite):
-#    walkRight = [pygame.image.load('R1E.png'), pygame.image.load('R2E.png'), pygame.image.load('R3E.png'), pygame.image.load('R4E.png'), pygame.image.load('R5E.png'), pygame.image.load('R6E.png'), pygame.image.load('R7E.png'), pygame.image.load('R8E.png'), pygame.image.load('R9E.png'), pygame.image.load('R10E.png'), pygame.image.load('R11E.png')]
-#    walkLeft= [pygame.image.load('L1E.png'), pygame.image.load('L2E.png'), pygame.image.load('L3E.png'), pygame.image.load('L4E.png'), pygame.image.load('L5E.png'), pygame.image.load('L6E.png'), pygame.image.load('L7E.png'), pygame.image.load('L8E.png'), pygame.image.load('L9E.png'), pygame.image.load('L10E.png'), pygame.image.load('L11E.png')]
-#
-#    def __init__(self,x,y,width,height,end):
-#        pygame.sprite.Sprite.__init__(self)
-#        self.x = x
-#        self.y = y
-#        self.width = width
-#        self.height = height
-#        self.path = [x, end]  #Onde começa e onde termina
-#        self.walkCount = 0
-#        self.vel = 3
-#        self.hitbox = (self.x + 17, self.y + 2, 31, 57)
-#        self.vida=10
-#        self.visible=True
-#    def update(self):
-#        self.move()
-#        if self.visible:
-#            if self.walkCount + 1 >= 33:
-#                self.walkCount = 0 
-#            if self.vel > 0:
-#                win.blit(self.walkRight[self.walkCount//3], (self.x,self.y))
-#                self.walkCount += 1
-#            else:
-#                win.blit(self.walkLeft[self.walkCount//3], (self.x,self.y))
-#                self.walkCount += 1
-#            pygame.draw.rect(win, (255,0,0), (self.hitbox[0], self.hitbox[1] - 20,50,10))                        #EXPLICAÇÃO
-#            pygame.draw.rect(win, (0,128,0), (self.hitbox[0], self.hitbox[1] - 20,50 - (5*(10 - self.vida)), 10))#EXPLICAÇÃO
-#            self.hitbox = (self.x + 17, self.y + 2, 31, 57)
-#    def move(self):
-#        if self.vel > 0:
-#            if self.x < self.path[1] + self.vel:
-#                self.x += self.vel
-#            else:
-#                self.vel = self.vel * -1
-#                self.x += self.vel
-#                self.walkCount = 0
-#        else:
-#            if self.x>self.path[0] - self.vel:
-#                self.x += self.vel
-#            else:
-#                self.vel = self.vel * -1
-#                self.x += self.vel
-#                self.walkCount = 0
-#    def hit(self):
-#        if self.vida>0:
-#            self.vida -=1
-#        else:
-#            self.visible = False
-#        print("hit")
-
 class projetil(pygame.sprite.Sprite):
     def __init__(self,x,y,pew):
         pygame.sprite.Sprite.__init__(self)
@@ -190,7 +96,6 @@ class projetil(pygame.sprite.Sprite):
         self.rect.centerx=x
         self.speedx=20
         #self.radius = radius
-        #self.color = color
         #self.facing = facing
 
     def update(self):
@@ -198,26 +103,11 @@ class projetil(pygame.sprite.Sprite):
         self.rect.centerx+=self.speedx
         if self.rect.centerx>width or self.rect.centerx<0:
             self.kill()
+            
+            
+            
+        
 
-'''    
-class projetil(pygame.sprite.Sprite):
-    def __init__(self,x,y,pew):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pew
-        self.image.set_colorkey((0,0,0))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.facing = facing
-        self.vel = 20 * facing
-           
-    def update(self,win):
-        self.rect.x += self.vel
-        if self.rect.x <0:
-            self.kill()
-'''        
-
- 
 class Platform(pygame.sprite.Sprite):
 
     def __init__(self,x,y,w,h, tipo):
@@ -288,19 +178,10 @@ for i in range (4):
             
 def RestaurarJanela():
     all_sprites.update()
-
     win.blit(bg, (0,0))
     all_sprites.draw(win)
-#    man.update()
-#    inimg.update(win)
-#    all_platforms.update()
-#    playergroup.update()
-#    bullets.update()
-    text=font.render("Score: " + str(score), 1, (255,215,0))
+    text=font.render("Lives: " + str(lives), 1, (255,215,0))
     win.blit(text,(750,10))
-#    for proj in projeteis:
-#        proj.update(win)
-#        all_sprites.add(proj)
     pygame.display.update()
 
 
@@ -310,14 +191,24 @@ playergroup.add(man)
 all_sprites.add(man)
 all_sprites.add(inimg)
 projeteis=[]
-score=0
+#score=0
 font = pygame.font.SysFont("comicsana",40,True)
 count=0
 run = True
 
 try:
+    
+    lives=4
     while run:
         clock.tick(27)
+        
+        hits = pygame.sprite.groupcollide(enemygroup, playergroup, True, False)
+     
+        if hits:
+            lives -= 1
+            
+            if lives == 0:
+               run= False
         
         hits = pygame.sprite.groupcollide(all_platforms, playergroup, False, False)
         for hit in hits:
@@ -326,23 +217,17 @@ try:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-    
-#        for proj in projeteis:
-#            if proj.rect.y - proj.radius < inimg.hitbox[1] + inimg.hitbox[3] and proj.rect.y + proj.radius > inimg.hitbox[1]:     #EXPLICAÇÃO
-#                if proj.rect.x + proj.radius > inimg.hitbox[0] and proj.rect.x - proj.radius < inimg.hitbox[0] + inimg.hitbox[2]: #EXPLICAÇÃO
-#                    inimg.hit()
-#                    score += 1
-#                    projeteis.pop(projeteis.index(proj))
-            
-                
+     
         keys = pygame.key.get_pressed()
         
         if keys[pygame.K_SPACE]:
-            #facing = -1
             bullet=projetil(man.rect.centerx,man.rect.bottom,pew)
             bullet.rect.centerx=man.rect.x
             bullet.rect.bottom=man.rect.y + 40
             projeteis.append(bullet)
+            
+            
+            
             all_sprites.add(bullet)
             bullets.add(bullet)
             
@@ -370,7 +255,7 @@ try:
                 man.jumpCount = 10
                 man.pulo = False
                 
-        if count == 1000:
+        if count == 100:
             
             en= enemy(random.randrange(0,WIDTH), random.randrange(0,HEIGHT))
             enemygroup.add(en)
