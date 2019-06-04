78f272d76fe430a40c5e2c29ce783d20b9a7829d
Julia <julia.r.martins@hotmail.com>
2019-06-04 09:22:53 -0300

    comentado

--- Game/img/pygame parte bia.py
+++ Game/img/pygame parte bia.py
@@ -1,23 +1,23 @@
-
+# Importando as bibliotecas que usaremos no decorrer do código.
 import random
 import pygame
 from os import path
 pygame.init()
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
+#Algumas informações sobre o jogo
+#cores
+WHITE = (255, 255, 255) 
 vermelhozinho = (190, 2, 20)
-rate=27
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
+#frames per second
+FPS=27
+#largura e altura da tela, respectivamente
 width=900
 height=600
+#determina o tamanho da tela 
 win = pygame.display.set_mode((900,600))
+#Nome do jogo 
 pygame.display.set_caption("Missão Polar")
-#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
+#indica a pasta que contém os sons e as figuras 
 walkRight = [pygame.image.load('papainoel3.png'), pygame.image.load('papainoel4.png'), pygame.image.load('papainoel5.png')]
 walkLeft = [pygame.image.load('papainoel0.png'), pygame.image.load('papainoel1.png'), pygame.image.load('papainoel2.png')]
 walkpoderR = [pygame.image.load('papainoel5p.png'), pygame.image.load('papainoel6p.png'), pygame.image.load('papainoel7p.png')]
@@ -33,33 +33,29 @@ boom=pygame.mixer.Sound(path.join(snd_dir, 'expl6.wav'))
 bgsong=pygame.mixer.Sound(path.join(snd_dir, 'bg.wav'))
 inicial=pygame.image.load('bbg.png')
 final=pygame.image.load('final.jpg').convert()
-
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-Bg=pygame.transform.scale(bg,(900,600))
-clock = pygame.time.Clock()
-telafinal=pygame.transform.scale(final,(900,600))
-
-
 vida=pygame.image.load('ra.jpg').convert()
+player_img = pygame.image.load('pegiga.png')
+#informações sobre a escala das imagens na tela 
 vida.set_colorkey((255,255,255))
 tamanhov=60,40
+Bg=pygame.transform.scale(bg,(900,600))
+telafinal=pygame.transform.scale(final,(900,600))
 vida0=pygame.transform.scale(vida,(tamanhov))
 vida1=pygame.transform.scale(vida,(tamanhov))
 vida2=pygame.transform.scale(vida,(tamanhov))
-
-
-
-player_img = pygame.image.load('pegiga.png') 
+# Variável para o ajuste de velocidade
+clock = pygame.time.Clock()
+#cria varios grupos
 all_sprites=pygame.sprite.Group()
 playergroup = pygame.sprite.Group()
 enemygroup = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 all_platforms=pygame.sprite.Group()
-
+# Classe Player que representa o jogador 
 class Player(pygame.sprite.Sprite):
+    # Construtor da classe Player
     def __init__(self,x,y,width,height):
         pygame.sprite.Sprite.__init__(self)
-
         self.image= pygame.transform.scale(char,(48,64))
         self.image.set_colorkey((0,0,0))
         self.rect=self.image.get_rect()
@@ -79,7 +75,7 @@ class Player(pygame.sprite.Sprite):
         self.sprite_left=0    
         self.sprite_invencivel=0
 
-        
+    # Função que faz a animação do player     
     def update_sprite(self):
         if self.direita:
             self.sprite_right += 1
@@ -113,27 +109,24 @@ class Player(pygame.sprite.Sprite):
             
             if self.pulo or self.parado and self.speedx==0:
                 self.image = pygame.transform.scale(char,(48,64))
-
+    # Função que atualiza a posição do player na tela
     def update(self):
-     
         self.rect.x += self.speedx
         self.rect.y+=self.speedy
-        
         if not self.parado:
-            self.speedy += 1 # Gravidade
-            
+            self.speedy += 1 # Gravidade 
         if self.speedx != 0:
             man.parado = False
-
         if self.rect.right > width:
             self.rect.right = width
         if self.rect.left < 0:
-            self.rect.left = 0
-            
-        if self.invencivel > 0:
+            self.rect.left = 0          
+        if self.invencivel > 0: # para o super poder durar 10 seg
             self.invencivel -= 1
 
+# Classe enemy que representa o inimigo 
 class enemy(pygame.sprite.Sprite):
+    # Construtor da classe enemy
     def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
         self.image = player_img
@@ -144,13 +137,11 @@ class enemy(pygame.sprite.Sprite):
         self.rect.x = x
         self.speedx = 5 
         self.speedy = 5
-
         self.herox = 0
         self.heroy = 0
-
         self.visible=True
         self.radius=27
-        
+    # Função que atualiza a posição do inimigo    
     def update(self):
         if self.visible:
             if(self.rect.x - 16 > self.herox):
@@ -164,13 +155,14 @@ class enemy(pygame.sprite.Sprite):
             
             elif(self.rect.y + 16 < self.heroy):
                 self.rect.y += self.speedy
-                
+    #Função que faz com que o inimigo va ate o Player            
     def sethero(self, x, y):
         self.herox = x
         self.heroy = y
 
-
+# Classe projetil que representa o tiro
 class projetil(pygame.sprite.Sprite):
+    # Construtor da classe projetil 
     def __init__(self,x,y,pew,facing):
         pygame.sprite.Sprite.__init__(self)
         self.image=pew
@@ -180,12 +172,14 @@ class projetil(pygame.sprite.Sprite):
         self.rect.centerx=x
         self.speedx=20 * facing
         self.facing = facing
+    # Função que atualiza a posição do projétil
     def update(self):
         self.rect.centerx += (self.speedx)
         if self.rect.centerx>width or self.rect.centerx<0:
             self.kill()
+# Classe Platform que representa as tres plataformas
 class Platform(pygame.sprite.Sprite):
-
+    # Construtor da classe Platform
     def __init__(self,x,y,w,h, tipo):
         pygame.sprite.Sprite.__init__(self)
         filename = ''
@@ -195,8 +189,6 @@ class Platform(pygame.sprite.Sprite):
             filename = 'snow_77.png'
         elif tipo== "middle":
            filename='snow_54.png'
-           
-           
         player_img=pygame.image.load(path.join(img_dir,filename)).convert()
         self.image=player_img 
         self.image=pygame.transform.scale(player_img,(w,h))    
@@ -206,7 +198,7 @@ class Platform(pygame.sprite.Sprite):
         self.rect.bottom=y
         self.image.set_colorkey((0,0,0))
    
-
+#colocando as plataformas em ordem na tela
 for i in range (4):
     if i==0:
         for i in range (4):
@@ -253,7 +245,7 @@ for i in range (4):
         all_sprites.add(p1)
         all_platforms.add(p1)
 
-            
+#função que atualiza todos os sprites, e mostra as vidas na tela            
 def RestaurarJanela(man):
 
     man.update_sprite() 
@@ -277,43 +269,43 @@ def RestaurarJanela(man):
 
     if man.invencivel > 0:
         
-        total_sec = man.invencivel//rate
+        total_sec = man.invencivel//FPS
         msg = "Acabou!!"
-        if man.invencivel>rate:
+        if man.invencivel>FPS:
             msg = "Poder ativado: 0:"+str(total_sec)
         text = font.render(msg, True, vermelhozinho)
         win.blit(text, (355, 50))
-
+    # Depois de desenhar, inverte o display
     pygame.display.update()
 
 
-   
+# cria nosso personagem  
 man=Player(1,450,64,64)
 playergroup.add(man)
 all_sprites.add(man)
-projeteis=[]
-font = pygame.font.SysFont("comicsana",40,True)
-count=0
-run = True
-font = pygame.font.SysFont("Somic Sans MS", 30)
 
+projeteis=[]
+#estabelece a fonte da 
+font = pygame.font.SysFont("Britannic Bold",40,True)
+count=0
+#inicia o looping principal
+run = True
  
-       
+#abre o arquivo em texto que guarda o High Score, lê e fecha      
 high_score_file = open("high_score_file.txt", "r")
 high_score = int(high_score_file.read())
 high_score_file.close()  
-
+#quantidade de flocos de neve
 numeroparticulas=100;
-
+#cria uma lista para fazer o mecanismo da neve se mexer
 neve_list=[]
-
+#mecanismo para cair a neve
 for i in range(numeroparticulas):
     x = random.randrange(0, 900)
     y = random.randrange(0, 600)
     neve_list.append([x,y])    
-
+#para iniciar o jogo clicando no botão do mouse
 end_it=False
-
 while not end_it:
     for event in pygame.event.get():
         if event.type==pygame.MOUSEBUTTONDOWN:
@@ -326,16 +318,17 @@ while not end_it:
     pygame.display.flip()
     
 try:
-    
+    #para iniciar o som
     bgsong.play(loops=-1)
+    #estabelece algumas variáveis
     score=0
     lives=3
     power=2
-
+    #iniciando o looping principal
     while run:
-      
-        clock.tick(rate)
-        
+        # Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        #collide da plataforma com o player
         hits = pygame.sprite.groupcollide(all_platforms, playergroup, False, False)
         for hit in hits:
             if man.speedy > 0:
@@ -348,14 +341,13 @@ try:
                 man.speedy = 0
                 man.pulo = False
          
-
+        #collide do player com o inimigo
         hits = pygame.sprite.groupcollide(enemygroup, playergroup, True, False, pygame.sprite.collide_circle)
-     
         if hits:
              if man.invencivel==0:
                  lives -= 1
 
-            
+        #abre o arquivo e escreve nele o score, retirando o score que estava escrito anteriomente, somente se tal score for maior que todos os anteriores    
         if lives == 0:
             if score>high_score:
                 high_score_file = open("high_score_file.txt", "w")
@@ -364,6 +356,8 @@ try:
                 high_score = score
             a= False
             pygame.mouse.get_pressed()
+            
+            #tela final do jogo
             while not a:
                 myfont=pygame.font.SysFont("Britannic Bold", 60)
                 vitorlindo=pygame.font.SysFont("Britannic Bold", 100)
@@ -379,21 +373,21 @@ try:
                     win.blit(nlabel,(width/2,height-550))
                     win.blit(b,(150,height-100))
                     win.blit(sco,(150,height-150))
+                    # Depois de desenhar, inverte o display.
                     pygame.display.flip()
                     run =False
            
             
-                        
+        #collide do inimigo com os tiros                
         hits =pygame.sprite.groupcollide(enemygroup,bullets , True, False, ) 
         if hits:
              boom.play()
              score+=1
              
-                 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+            #o que apertar para atirar, pular, andar para a esquerda e para a direita
             keys = pygame.key.get_pressed()
             if keys[pygame.K_SPACE]:
                 if man.esquerda:
@@ -405,20 +399,12 @@ try:
                     man.pulo = True
                     man.speedy = -20
                     man.parado = False
-                    
-            if keys[pygame.K_s]:
-                if power>0:
-            
-                    man.invencivel=270
-                 
-                                    
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_a:
                     man.speedx -= man.vel
                     man.direita = False
                     man.esquerda= True
-                    man.parado = False
-                    
+                    man.parado = False                   
                 if event.key == pygame.K_d:
                     man.speedx += man.vel
                     man.direita = True
@@ -432,53 +418,46 @@ try:
                     projeteis.append(bullet)
                     all_sprites.add(bullet)
                     bullets.add(bullet)
-          
+                #apertar "s" para ficar invencivel
                 if power>0:
                      if event.key == pygame.K_s:
-                         man.invencivel= rate * 10
+                         man.invencivel= FPS * 10
                          power-=1
-                
+            #quando retira o dedo da tecla, ele para de fazer o comando que estava sendo executado  
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_a:
                     man.speedx = 0
                 if event.key == pygame.K_d:
-                    man.speedx = 0
-
-
-                
-
-
+                    man.speedx = 0           
+        #mecanismo de aparição de inimigos
         if score>20: 
-            if count == 50:
-                
+            if count == 50:             
                 en= enemy(random.randrange(0,width), random.randrange(0,height))
                 enemygroup.add(en)
                 all_sprites.add(en)
                 count=0
-
         else:
-            if count == 80:
-                
+            if count == 80:               
                 en= enemy(random.randrange(0,width), random.randrange(0,height))
                 enemygroup.add(en)
                 all_sprites.add(en)
                 count=0
-
         count+=1
-                
         RestaurarJanela(man)
+        #mecanismo para o inimigo seguir o personagem
         for en in enemygroup:
             en.sethero(man.rect.x, man.rect.y)
-        
+        #para a neve se mover 
         for point in neve_list:
             point[1]+=1
             pygame.draw.circle(win, WHITE, point, 2)
-
             if(point[1] >= height):
                 point[0] = random.randrange(0, 900)
                 point[1] = random.randrange(-10, -5)
+        # Depois de desenhar, inverte o display.
         pygame.display.flip()
+#finaliza o jogo 
 finally:
     pygame.quit()
-#    sys.exit()
+    #evita travamentos
     quit()
